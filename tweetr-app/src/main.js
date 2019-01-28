4db9c6b1a1b80d4ffc57272a77b0cefb8d55005d
Fadli Ramadhan <fadlir93@gmail.com>
2019-01-28 16:28:07 +0700

    Install Vee-Validate

--- tweetr-app/src/main.js
+++ tweetr-app/src/main.js
@@ -4,10 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import VeeValidate from 'vee-validate'
 
 window.axios = axios
 axios.defaults.baseURL = 'http://127.0.0.1:3333'
 Vue.config.productionTip = false
+//register vee validate plugin
+Vue.use(VeeValidate)
 
 /* eslint-disable no-new */
 new Vue({
